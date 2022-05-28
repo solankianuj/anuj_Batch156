ca5c8224735139f8013d09991e8c0151806971a6
solanki <anujaj2222.gmail.com>
2022-05-28 18:31:44 +0530

    employe wage computation

---
+++ src/computaionOfWage/WageComputaion.java
@@ -0,0 +1,8 @@
+package computaionOfWage;
+
+public class WageComputaion {
+	public static void main(String[] args) {
+		System.out.println("well come to employe wage computation program ");
+	}
+
+}
