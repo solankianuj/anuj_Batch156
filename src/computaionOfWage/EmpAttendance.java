6890c8e845d70d50259ba093f04c68956defaabc
solanki <anujaj2222.gmail.com>
2022-05-28 19:48:53 +0530

    checkimg employe attendance

---
+++ src/computaionOfWage/EmpAttendance.java
@@ -0,0 +1,20 @@
+package computaionOfWage;
+
+public class EmpAttendance {
+public static void main(String[] args) {
+	int full_time=1;
+	double empcheck = Math.floor(Math.random()*10) % 2 ;
+	
+	System.out.println(+empcheck);
+	if (empcheck == 1)
+	{
+			System.out.println("employe is present");
+		}
+		else		System.out.println("employe is absent");
+
+	
+}	
+	
+		
+}
+
